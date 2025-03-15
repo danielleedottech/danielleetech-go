56a0fae13807c2a742e6621a90fa0a7ebea9cede
Daniel Lee <danlee1996@proton.me>
2025-03-14 20:24:42 -0600

    adding printlns for ports

--- main.go
+++ main.go
@@ -18,8 +18,10 @@ func RunServer() {
 	var err error
 
 	if os.Getenv("ENV") == "production" {
+		fmt.Println("running in port 80")
 		err = http.ListenAndServe(":80", nil)
 	} else {
+		fmt.Println("running in port 80")
 		err = http.ListenAndServe(":8080", nil)
 	}
 
